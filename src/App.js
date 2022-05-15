d4615f76272f1e7e3feff253aff0238452562529
marufsll <maruf.bhuiyan@shadhinlab.com>
2022-05-15 15:23:24 +0600

    routes add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import Home from "./app/home/home";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './app/about/about';
 
 function App() {
     return (
         <div>
             <Helmet>
                 <title>App Title</title>
-                <meta name="description" content="App Description"/>
-                <meta name="theme-color" content="#008f68"/>
+                <meta name="og:title" content="React share" />
+                <meta name="description" content="App Description" />
+                <meta name="theme-color" content="#008f68" />
             </Helmet>
-            <Home/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/about" element={<About />} />
+                    <Route path="/" exact
+                        element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+
         </div>
     );
 }
